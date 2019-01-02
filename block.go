55efdc0f0554a5f9bd94a671772b49922a413564
TaBa Trung <trungtaba@gmail.com>
2019-01-02 22:30:12 +0700

    POW

---
+++ block.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"strconv"
+	"time"
+)
+
+// Block keeps block headers
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
+
+// SetHash calculates and sets block hash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+// Serialize serializes the block
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	encoder.Encode(b)
+
+	return result.Bytes()
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	decoder.Decode(&block)
+
+	return &block
+}
+
+// NewBlock creates and returns Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash
+	block.Nonce = nonce
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
---
+++ proofOfWork.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const DIFFICULTY = 3
+
+// ProofOfWork represents a proof-of-work
+type ProofOfWork struct {
+	block *Block
+}
+
+// NewProofOfWork builds and returns a ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+	return &ProofOfWork{b}
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			[]byte(strconv.Itoa(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+// Run performs a proof-of-work
+func (pow *ProofOfWork) Run() (int, []byte) {
+	nonce := 0
+	var hash [32]byte
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for true {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		if strings.HasPrefix(hex.EncodeToString(hash[:]), strings.Repeat("0", DIFFICULTY)) {
+			break
+		}
+		nonce++
+	}
+	return nonce, hash[:]
+}
+
+// Validate validates block's PoW
+func (pow *ProofOfWork) Validate() bool {
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	return strings.HasPrefix(hex.EncodeToString(hash[:]), strings.Repeat("0", DIFFICULTY))
+}
